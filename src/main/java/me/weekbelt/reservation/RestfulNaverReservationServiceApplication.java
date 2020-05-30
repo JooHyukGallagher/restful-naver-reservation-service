5c464234f82a634e863cf365a0ebe79ddb9860b9
joohyuk <vfrvfr4207@gmail.com>
2020-05-30 22:42:46 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/reservation/RestfulNaverReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulNaverReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulNaverReservationServiceApplication.class, args);
+    }
+
+}

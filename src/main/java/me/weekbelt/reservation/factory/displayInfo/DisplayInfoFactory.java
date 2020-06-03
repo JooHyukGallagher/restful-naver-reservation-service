c1e1aeb6ea27f770999f6b3d14587a72c0e8580c
joohyuk <vfrvfr4207@gmail.com>
2020-06-03 13:42:34 +0900

    refactoring displayInfo

--- src/main/java/me/weekbelt/reservation/factory/displayInfo/DisplayInfoFactory.java
+++ src/main/java/me/weekbelt/reservation/factory/displayInfo/DisplayInfoFactory.java
@@ -3,7 +3,11 @@ package me.weekbelt.reservation.factory.displayInfo;
 import me.weekbelt.reservation.domain.displayInfo.DisplayInfo;
 import me.weekbelt.reservation.domain.productImage.ProductImage;
 import me.weekbelt.reservation.factory.product.ProductFactory;
+import me.weekbelt.reservation.web.form.displayInfo.DisplayInfoResponse;
+import me.weekbelt.reservation.web.form.displayInfoImage.DisplayInfoImageDto;
 import me.weekbelt.reservation.web.form.product.ProductDto;
+import me.weekbelt.reservation.web.form.productImage.ProductImageDto;
+import me.weekbelt.reservation.web.form.productPrice.ProductPriceDto;
 
 import java.util.List;
 
@@ -14,5 +18,17 @@ public class DisplayInfoFactory {
         ProductDto productDto = ProductFactory.displayInfoToProductDto(displayInfo);
         productDto.addFileId(productImages.get(0).getFileInfo().getId());
         return productDto;
+    }
+
+    public static DisplayInfoResponse makeDisplayInfoResponse(ProductDto product, List<ProductImageDto> productImages,
+                                                        List<DisplayInfoImageDto> displayInfoImages,
+                                                        Double avgScore, List<ProductPriceDto> productPrices) {
+        return DisplayInfoResponse.builder()
+                .product(product)
+                .productImages(productImages)
+                .displayInfoImages(displayInfoImages)
+                .avgScore(avgScore)
+                .productPrices(productPrices)
+                .build();
     }
 }
--- src/main/java/me/weekbelt/reservation/web/controller/DisplayInfoApiController.java
+++ src/main/java/me/weekbelt/reservation/web/controller/DisplayInfoApiController.java
@@ -1,11 +1,8 @@
 package me.weekbelt.reservation.web.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.weekbelt.reservation.domain.ImageType;
-import me.weekbelt.reservation.domain.productImage.ProductImage;
-import me.weekbelt.reservation.domain.productImage.ProductImageRepository;
-import me.weekbelt.reservation.domain.productPrice.ProductPriceRepository;
 import me.weekbelt.reservation.domain.reservationUserComment.ReservationUserCommentRepository;
+import me.weekbelt.reservation.factory.displayInfo.DisplayInfoFactory;
 import me.weekbelt.reservation.service.DisplayInfoImageService;
 import me.weekbelt.reservation.service.ProductImageService;
 import me.weekbelt.reservation.service.ProductPriceService;
@@ -14,6 +11,7 @@ import me.weekbelt.reservation.web.form.displayInfo.DisplayInfoResponse;
 import me.weekbelt.reservation.web.form.displayInfoImage.DisplayInfoImageDto;
 import me.weekbelt.reservation.web.form.product.ProductDto;
 import me.weekbelt.reservation.web.form.productImage.ProductImageDto;
+import me.weekbelt.reservation.web.form.productPrice.ProductPriceDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,21 +33,12 @@ public class DisplayInfoApiController {
     @GetMapping("/v1/displayinfos/{displayInfoId}")
     public DisplayInfoResponse displayInfo(@PathVariable Long displayInfoId){
         ProductDto product = productService.findProductDtoByDisplayInfoId(displayInfoId);
-        List<ProductImageDto> productImages = productImageService
-                .findProductImageDtoList(product.getId());
-        List<DisplayInfoImageDto> displayInfoImages = displayInfoImageService
-                .findDisplayInfoImageDtoByDisplayInfoId(displayInfoId);
+        List<ProductImageDto> productImages = productImageService.findProductImageDtoList(product.getId());
+        List<DisplayInfoImageDto> displayInfoImages = displayInfoImageService.findDisplayInfoImageDtoByDisplayInfoId(displayInfoId);
+        Double avgScore = reservationUserCommentRepository.commentAverageScoreByProductId(product.getId());
+        List<ProductPriceDto> productPrices = productPriceService.findProductPriceDtoListByProductId(product.getId());
 
-        return makeDisplayInfoResponse(product, productImages, displayInfoImages);
+        return DisplayInfoFactory.makeDisplayInfoResponse(product, productImages, displayInfoImages, avgScore, productPrices);
     }
 
-    private DisplayInfoResponse makeDisplayInfoResponse(ProductDto product, List<ProductImageDto> productImages, List<DisplayInfoImageDto> displayInfoImages) {
-        return DisplayInfoResponse.builder()
-                .product(product)
-                .productImages(productImages)
-                .displayInfoImages(displayInfoImages)
-                .avgScore(reservationUserCommentRepository.commentAverageScoreByProductId(product.getId()))
-                .productPrices(productPriceService.findProductPriceDtoListByProductId(product.getId()))
-                .build();
-    }
 }
